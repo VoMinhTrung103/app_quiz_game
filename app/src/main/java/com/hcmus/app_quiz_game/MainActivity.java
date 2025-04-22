ca035bb8120b3f98f862e4e017b90dfd9cbd11d8
Vo_Minh_Trung <vominhtrung9617@gmail.com>
2025-04-22 08:23:07 +0700

    Initial commit for Quiz Game app

---
+++ app/src/main/java/com/hcmus/app_quiz_game/MainActivity.java
@@ -0,0 +1,12 @@
+package com.hcmus.app_quiz_game;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstaceState){
+        super.onCreate(saveInstaceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/hcmus/app_quiz_game/ThirdActivity.java
@@ -0,0 +1,10 @@
+package com.hcmus.app_quiz_game;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class ThirdActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
